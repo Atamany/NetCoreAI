55c5288deb24130fab6bd682d5738150e59a4e4e
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-19 18:19:16 +0300

    Proje 7: Tesseract OCR ile Resim Üzerindeki Yazıları Okuma

---
+++ NetCoreAI.Project07_TesseractOcr/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Karakter Okumasu Yapılacak Resim Yolu:");
+        string imagePath = Console.ReadLine();
+        string tessDataPath = @"C:\tessdata";
+        try
+        {
+            using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Resimden Okunan Metin:");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+}
