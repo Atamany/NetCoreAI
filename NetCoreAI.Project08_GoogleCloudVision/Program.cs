a7ed05c86d8b3ae4df4857900a1d0950bf6f733c
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-19 19:32:14 +0300

    Proje 8: Google Cloud Vision Api ile Resim Üzerindeki Yazıları Okuma

---
+++ NetCoreAI.Project08_GoogleCloudVision/Program.cs
@@ -0,0 +1,31 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Resim Yolunu Giriniz: ");
+        string imagePath = Console.ReadLine();
+        string credentialPath = @"";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimdeki Metinler:");
+            Console.WriteLine();
+            foreach (var annotation in response)
+            {
+                if (!string.IsNullOrEmpty(annotation.Description))
+                {
+                    Console.WriteLine(annotation.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+}
