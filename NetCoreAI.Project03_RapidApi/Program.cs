4c0025e9affe56e13935cd2a2e14ac953cdb5741
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-19 15:50:07 +0300

    Proje 3: Dışarıdan Harici Apiler ile Çalışmak (Rapid Api)

---
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using NetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "f3199434e5msh58ee98c4e68e750p14cb70jsn56545f8ecbb0" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach (var series in apiSeriesViewModels)
+    {
+        Console.WriteLine($"Sıra: {series.rank}, Başlık: {series.title}, Puan: {series.rating}, Yıl: {series.year}");
+    }
+}
