c72249acd16f811b411a16e163e487fd5b5c3495
Yiğit ATAMAN <yigitataman1@gmail.com>
2026-05-14 13:18:50 +0300

    Proje 10: Text To Speech

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = 0;
+        Console.WriteLine("Metni sesli olarak okumak istediğiniz cümleyi giriniz.");
+        string inputText = Console.ReadLine();
+        if (!string.IsNullOrEmpty(inputText))
+        {
+            speechSynthesizer.Speak(inputText);
+        }
+    }
+}
